cd6668ee2b43eb20f5086f04e7875bf8a7811ef6
Mattia Cantalù <mattiacantalu@me.com>
2017-04-20 14:47:22 +0200

    Added window alert and refresh button

--- cookies.js
+++ cookies.js
@@ -1,2 +1,10 @@
-allCookies = document.cookie;
-console.log(allCookies);
+alert(listCookies())
+
+function listCookies() {
+    var theCookies = document.cookie.split(';');
+    var aString = '';
+    for (var i = 1 ; i <= theCookies.length; i++) {
+        aString += i + ' ' + theCookies[i-1] + "\n";
+    }
+    return aString;
+}
