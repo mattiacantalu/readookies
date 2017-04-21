345a2bd62b9c5158a415bad635f7c7ab381b8775
Mattia Cantalù <mattiacantalu@me.com>
2017-04-21 11:21:37 +0200

    Move code do index, added header list

--- cookies.js
+++ cookies.js
@@ -1,10 +1,6 @@
-alert(listCookies())
-
-function listCookies() {
-    var theCookies = document.cookie.split(';');
-    var aString = '';
-    for (var i = 1 ; i <= theCookies.length; i++) {
-        aString += i + ' ' + theCookies[i-1] + "\n";
-    }
-    return aString;
+var theCookies = document.cookie.split(';');
+var aString = '';
+for (var i = 1 ; i <= theCookies.length; i++) {
+    aString += i + ' ' + theCookies[i-1] + "\n";
 }
+alert(aString);
